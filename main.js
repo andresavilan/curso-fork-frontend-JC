078a6e14db67ebaaf6c8081d05cabdcf99e26f84
Andres Avilan <andresfavilan@hotmail.com>
2023-02-02 16:44:42 -0500

    Added the shopping cart and products using JC DOM

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const menuIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuShoppingCart = document.querySelector( '.navbar-shopping-cart')
 const aside = document.querySelector('.product-detail')
+const cardsContainer = document.querySelector('.cards-container')
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuIcon.addEventListener('click', toggleMobileMenu)
@@ -24,6 +25,65 @@ function toggleMenuShoppingCart() {
     desktopMenu.classList.add('inactive')
     mobileMenu.classList.add('inactive')
     aside.classList.toggle('inactive')
-
-    
 }
+
+const productList = [];
+
+productList.push ({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push ({
+    name: 'skate',
+    price: 60,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+productList.push ({
+    name: 'Ball',
+    price: 80,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+})
+
+
+function renderProducts(nameArray) {
+    
+    for (product of nameArray) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p')
+        productPrice.innerText= '$' + product.price
+
+        const productName = document.createElement('p')
+        productName.innerText = product.name
+
+        productInfoDiv.append(productPrice, productName)
+
+        const productFigure = document.createElement('figure')
+        const productImgCart = document.createElement('img')
+        productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg')
+
+        productFigure.append(productImgCart)
+
+        productInfo.append(productInfoDiv, productFigure)
+
+        productCard.append(productImg, productInfo)
+
+        cardsContainer.append (productCard)
+    }
+}
+
+console.log(productList);
+
+renderProducts(productList)
