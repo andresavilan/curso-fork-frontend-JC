b51ab36364026ef759163981355ad0e541a61037
Andres Avilan <andresfavilan@hotmail.com>
2023-02-17 14:52:16 -0500

    agregado el aside de product details

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector('.desktop-menu')
 const menuIcon = document.querySelector('.menu')
 const mobileMenu = document.querySelector('.mobile-menu')
 const menuShoppingCart = document.querySelector( '.navbar-shopping-cart')
-const aside = document.querySelector('.product-detail')
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer')
 const cardsContainer = document.querySelector('.cards-container')
 
 navEmail.addEventListener('click', toggleDesktopMenu);
@@ -12,19 +12,19 @@ menuShoppingCart.addEventListener( 'click', toggleMenuShoppingCart)
 
 
 function toggleDesktopMenu() {
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
     mobileMenu.classList.toggle('inactive');
 }
 
 function toggleMenuShoppingCart() {
     desktopMenu.classList.add('inactive')
     mobileMenu.classList.add('inactive')
-    aside.classList.toggle('inactive')
+    shoppingCartContainer.classList.toggle('inactive')
 }
 
 const productList = [];
